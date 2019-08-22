05f230aeef79ca2a00fdab8327916a6c34146e19
alvinisasi <azraqazravin@gmail.com>
2019-08-22 20:30:48 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap');
+
+#app {
+  font-family: 'Open Sans', 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
